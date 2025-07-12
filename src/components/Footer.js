609017d4ccb04198000afb0794d50c2501ce834e
ilam15 <ilamsaravan@gmail.com>
2025-07-12 13:53:11 +0530

    First Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import "./Footer.css"
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <span className="footer-icon">©</span> 2025&nbsp;
+        <a href="http://localhost:3000/" target="_blank" rel="noopener noreferrer" className="footer-link">
+          Tododify
+        </a>
+      </p>
+    </footer>
+  )
+}
+
+
